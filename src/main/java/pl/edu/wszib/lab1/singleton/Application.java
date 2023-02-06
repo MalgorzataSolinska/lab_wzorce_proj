6c2efd862ec5c87d9df8d4693a623b919b9fc189
masolins <gobaran@gmail.com>
2023-02-06 14:42:02 +0100

    singletown wielowatkowosc - odstawowa implementacja

--- src/main/java/pl/edu/wszib/lab1/singleton/Application.java
+++ src/main/java/pl/edu/wszib/lab1/singleton/Application.java
@@ -1,9 +1,30 @@
 package pl.edu.wszib.lab1.singleton;
 
+import java.net.ConnectException;
 import java.time.LocalDateTime;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Application {
-  public static void main(String[] args) {
 
+  static ConnectionLazy instance1;
+  public static void main(String[] args) {
+    ExecutorService executorService = Executors.newSingleThreadExecutor();
+    executorService.submit(new Runnable() {
+                             @Override
+                             public void run() {
+                               instance1 = ConnectionLazy.getInstance();
+                             }
+    });
+    ConnectionLazy instance = ConnectionLazy.getInstance();
+  if(instance == instance1) {
+    System.out.println(instance);
+    System.out.println(instance1);
+    System.out.println("Are the same");
+  }else{
+    System.out.println(instance);
+    System.out.println(instance1);
+    System.out.println("Instances are different");
+  }
   }
 }
--- src/main/java/pl/edu/wszib/lab1/singleton/ConnectionLazy.java
+++ src/main/java/pl/edu/wszib/lab1/singleton/ConnectionLazy.java
@@ -5,9 +5,16 @@ public final class ConnectionLazy {
   private static ConnectionLazy INSTANCE;
 
   private ConnectionLazy() {
+    try {
+      Thread.sleep(100);
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
   }
-//brak pewnosci czy zostanie utworzona jedna instancja, nie jest zapewniona obsługa jednowątkowa
-  public static ConnectionLazy getInstance(){
+//brak pewnosci czy zostanie utworzona jedna instancja,
+// nie jest zapewniona obsługa jednowątkowa
+  //synchronized zapewnia zakolejkowanie sie watkow
+  public synchronized static ConnectionLazy getInstance(){
     if(INSTANCE == null){
       INSTANCE = new ConnectionLazy();
     }
