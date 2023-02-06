fab192a5d1fa27ab4327a4a32c9c9411ea373f4d
masolins <gobaran@gmail.com>
2023-02-06 13:43:37 +0100

    fabryka

--- src/main/java/pl/edu/wszib/lab1/builder/record/Application.java
+++ src/main/java/pl/edu/wszib/lab1/builder/record/Application.java
@@ -7,8 +7,30 @@ import pl.edu.wszib.lab1.builder.MyRecord;
 public class Application {
 
   public static void main(String[] args) {
-    Person male = Person.male("Grzegorz", "Solins", 30, new Address("Krakow", "dd", "ddda3", "asdjhbf"));
-    Person female = Person.female("Malgorzata", "Solins", 30, new Address("Krakow", "dd", "ddda3", "asdjhbf"));
+    Person male = Person.male(
+        "Grzegorz",
+        "Solins",
+        30,
+        new Address("Krakow", "dd", "ddda3", "asdjhbf"));
+    Person female = Person.female(
+        "Malgorzata",
+        "Solins",
+        30,
+        new Address("Krakow", "dd", "ddda3", "asdjhbf"));
+
+
+    final PersonFactory personFactory = new PersonFactory();
+    personFactory.male(
+        "Grzegorz",
+        "Solins",
+        30,
+        new Address("Krakow", "dd", "ddda3", "asdjhbf"));
+
+    personFactory.female(
+        "Malgorzata",
+        "Solins",
+        30,
+        new Address("Krakow", "dd", "ddda3", "asdjhbf"));
 
   }
 }
--- src/main/java/pl/edu/wszib/lab1/builder/record/PersonFactory.java
+++ src/main/java/pl/edu/wszib/lab1/builder/record/PersonFactory.java
@@ -1,4 +1,13 @@
 package pl.edu.wszib.lab1.builder.record;
 
+import pl.edu.wszib.lab1.builder.Address;
+
 public class PersonFactory {
+  public Person male(String firstName, String lastName, int age, Address address) {
+    return Person.male(firstName, lastName,  age,  address);
+  }
+
+  public Person female(String firstName, String lastName, int age, Address address) {
+    return Person.female(firstName, lastName, age, address);
+  }
 }
